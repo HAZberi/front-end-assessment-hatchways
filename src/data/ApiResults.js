ce4405d1d9e2b89ca3522edbfe0e933ec98062d1
Hassaan Zuberi <mhazuberi@gmail.com>
2021-06-12 19:29:40 -0600

    basic app styling complete

--- src/data/ApiResults.js
+++ src/data/ApiResults.js
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from "react";
 import hatchwaysApi from "../api/hatchways";
-import StudentCard from "../components/StudentCard"
+import StudentCard from "../components/StudentCard";
+import { Grid, Paper } from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  appBackground: {
+    backgroundColor: theme.palette.common.whitesmoke,
+    width: "100%",
+    height: "100%",
+  },
+  dataContainer: {
+    marginBottom: "7vh",
+    marginTop: "7vh",
+    maxHeight: "86vh",
+    width: "70%",
+    overflowY: "scroll",
+    borderRadius: "10px",
+  },
+}));
 
 const ApiResults = () => {
+    //To access Styles
+  const classes = useStyles();
+    //React States
+    //We are not using Redux or Flux since state management is simple
+    //and app's component architecture is relatively flat.
   const [students, setStudents] = useState([]);
 
   const getStudentData = async () => {
@@ -20,11 +43,15 @@ const ApiResults = () => {
   }, []);
 
   return (
-    <div>
-      {students.map((student) => (
-          <StudentCard key={student.id} data={student} />
-      ))}
-    </div>
+    <Grid container className={classes.appBackground}>
+      <Grid item container direction="column" alignItems="center">
+        <Paper variant="elevation" elevation={2} className={classes.dataContainer}>
+          {students.map((student) => (
+            <StudentCard key={student.id} data={student} />
+          ))}
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
 
