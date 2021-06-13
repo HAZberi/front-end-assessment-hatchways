e131a70b5fee6a62db44d77bbf3d03a31eeefb6d
Hassaan Zuberi <mhazuberi@gmail.com>
2021-06-12 19:32:45 -0600

    basic app styling non-responsive complete

--- src/data/ApiResults.js
+++ src/data/ApiResults.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import hatchwaysApi from "../api/hatchways";
 import StudentCard from "../components/StudentCard";
 import { Grid, Paper } from "@material-ui/core";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   appBackground: {
